6bcf08a4745fc084b172280247b54f7d8c9c75ed
Toni Zeidler <toni.z@gmx.de>
2022-10-21 15:01:36 +0200

    Add some changes

--- src/main/java/de/egosanto/ms/movieinfo/repository/MovieInfoRepository.java
+++ src/main/java/de/egosanto/ms/movieinfo/repository/MovieInfoRepository.java
@@ -6,9 +6,13 @@ import de.egosanto.ms.movieinfo.models.Movie;
 
 /**
  * CRUD Services Inteface for Movie Domain
- *
+ * C = Create
+ * R = Read oder Retrieve
+ * U = Update
+ * D = Delete oder Destroy
+ * @see 'https://en.wikipedia.org/wiki/Create,_read,_update_and_delete'
  * @author Toni Zeidler
- * s
+ *
  */
 public interface MovieInfoRepository extends CrudRepository<Movie, Long> {
 
