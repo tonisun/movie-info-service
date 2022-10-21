7df7d5287fe85e5ee254f9e8e162098a202cc4e2
Toni Zeidler <toni.z@gmx.de>
2022-10-21 13:53:38 +0200

    Add -s nl cleaning

--- src/main/java/de/egosanto/ms/movieinfo/repository/MovieInfoRepository.java
+++ src/main/java/de/egosanto/ms/movieinfo/repository/MovieInfoRepository.java
@@ -1,10 +1,6 @@
 package de.egosanto.ms.movieinfo.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import de.egosanto.ms.movieinfo.models.Movie;
 
